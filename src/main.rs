9c6753747422dfb1825f8c15d339a128815f099a
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-10-26 17:01:35 +0000

    Implement sorting

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ extern crate chrono_tz;
 use std::collections::HashMap;
 use chrono_tz::Tz;
 use clap::{App, Values};
-use chrono::{DateTime, Utc, Duration, Timelike};
+use chrono::{DateTime, Utc, Duration, Timelike, Datelike};
+use std::cmp::Ordering;
+
+#[derive(Debug)]
+struct TimezoneHours<'a> {
+    name: &'a str,
+    hours: Vec<String>,
+    tz: Tz,
+}
 
 fn get_timezones(cmdline_tzs: Option<Values<'_>>) -> HashMap<&str, Tz> {
     let mut timezones = HashMap::new();
@@ -48,33 +56,6 @@ fn get_utc_date(cmdline_date: Option<&str>) -> DateTime<Utc> {
     }
 }
 
-fn get_hours_matrix(
-    tzs: HashMap<&str, Tz>,
-    date: DateTime<Utc>,
-    span: i32,
-) -> HashMap<&str, Vec<u32>> {
-    let half_span = span - span / 2;
-    let mut matrix = HashMap::new();
-    for (tz_str, tz) in &tzs {
-        let mut hours = Vec::new();
-        for i in 0..span {
-            let offset = i64::from(i - half_span);
-            let converted = date.with_timezone(tz);
-            let mut shifted = converted
-                .checked_add_signed(Duration::hours(offset))
-                .unwrap();
-            if offset < 0 {
-                shifted = converted
-                    .checked_sub_signed(Duration::hours(-offset))
-                    .unwrap();
-            }
-            hours.push(shifted.hour())
-        }
-        matrix.insert(*tz_str, hours);
-    }
-    matrix
-}
-
 fn get_span(cmdline_span: Option<&str>) -> i32 {
     let default = 12;
     match cmdline_span {
@@ -96,13 +77,73 @@ fn get_span(cmdline_span: Option<&str>) -> i32 {
     }
 }
 
+fn calculate_timezone_hours(
+    tzs: HashMap<&str, Tz>,
+    date: DateTime<Utc>,
+    span: i32,
+) -> Vec<TimezoneHours> {
+    let half_span = (span / 2) - 1;
+    let mut tzhours = Vec::new();
+    for (tz_str, tz) in &tzs {
+        let mut hours = Vec::new();
+        for i in 0..span {
+            let offset = i64::from(i - half_span);
+            let utc_day = date.day();
+            let converted = date.with_timezone(tz);
+            let mut shifted = converted
+                .checked_add_signed(Duration::hours(offset))
+                .unwrap();
+            if offset < 0 {
+                shifted = converted
+                    .checked_sub_signed(Duration::hours(-offset))
+                    .unwrap();
+            }
+            let mut sfx = " ";
+            if shifted.day() < utc_day {
+                sfx = "-"
+            } else if shifted.day() > utc_day {
+                sfx = "+"
+            }
+            if offset == 0 {
+                // current hour!
+                hours.push(format!("| {}{}|", shifted.hour(), sfx));
+            } else {
+                hours.push(format!(" {}{}", shifted.hour(), sfx));
+            }
+        }
+        tzhours.push(TimezoneHours {
+            name: tz_str,
+            tz: *tz,
+            hours: hours,
+        });
+    }
+    tzhours
+}
+
 fn main() {
     let config_yaml = clap::load_yaml!("args.yaml");
     let matches = App::from(config_yaml).get_matches();
-    let matrix = get_hours_matrix(
+    let mut tzhours = calculate_timezone_hours(
         get_timezones(matches.values_of("timezones")),
         get_utc_date(matches.value_of("date")),
         get_span(matches.value_of("span")),
     );
-    println!("Hello, world!: {:?}", matrix);
+    let inverse = matches.is_present("inverseorder");
+    if !matches.is_present("noorder") {
+        let date = chrono::Utc::now();
+        tzhours.sort_by(|a, b| {
+            let d_a = date.with_timezone(&a.tz).timestamp();
+            let d_b = date.with_timezone(&b.tz).timestamp();
+            let (less, greater) = if inverse {
+                (Ordering::Less, Ordering::Greater)
+            } else {
+                (Ordering::Greater, Ordering::Less)
+            };
+            if d_a == d_b {
+                return Ordering::Equal;
+            }
+            if d_a < d_b { less } else { greater }
+        });
+    }
+    println!("Hello, world!: {:?}", tzhours);
 }
