4b05ff0355cacad3116920ca4e93ec928379e873
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-10-26 12:55:29 +0000

    compute the time matrix

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate chrono_tz;
 use std::collections::HashMap;
 use chrono_tz::Tz;
 use clap::{App, Values};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Duration, Timelike};
 
 fn get_timezones(cmdline_tzs: Option<Values<'_>>) -> HashMap<&str, Tz> {
     let mut timezones = HashMap::new();
@@ -48,11 +48,61 @@ fn get_utc_date(cmdline_date: Option<&str>) -> DateTime<Utc> {
     }
 }
 
+fn get_hours_matrix(
+    tzs: HashMap<&str, Tz>,
+    date: DateTime<Utc>,
+    span: i32,
+) -> HashMap<&str, Vec<u32>> {
+    let half_span = span - span / 2;
+    let mut matrix = HashMap::new();
+    for (tz_str, tz) in &tzs {
+        let mut hours = Vec::new();
+        for i in 0..span {
+            let offset = i64::from(i - half_span);
+            let converted = date.with_timezone(tz);
+            let mut shifted = converted
+                .checked_add_signed(Duration::hours(offset))
+                .unwrap();
+            if offset < 0 {
+                shifted = converted
+                    .checked_sub_signed(Duration::hours(-offset))
+                    .unwrap();
+            }
+            hours.push(shifted.hour())
+        }
+        matrix.insert(*tz_str, hours);
+    }
+    matrix
+}
+
+fn get_span(cmdline_span: Option<&str>) -> i32 {
+    let default = 12;
+    match cmdline_span {
+        Some(span) => {
+            match span.parse::<i32>() {
+                Ok(val) => val,
+                Err(e) => {
+                    println!(
+                        "Cannot parse {} as int: {}. Using default {}",
+                        span,
+                        e,
+                        default
+                    );
+                    default
+                }
+            }
+        }
+        None => default,
+    }
+}
+
 fn main() {
     let config_yaml = clap::load_yaml!("args.yaml");
     let matches = App::from(config_yaml).get_matches();
-    let _timezones = get_timezones(matches.values_of("timezones"));
-    let date = get_utc_date(matches.value_of("date"));
-    println!("Using date: {}", date);
-    println!("Hello, world!");
+    let matrix = get_hours_matrix(
+        get_timezones(matches.values_of("timezones")),
+        get_utc_date(matches.value_of("date")),
+        get_span(matches.value_of("span")),
+    );
+    println!("Hello, world!: {:?}", matrix);
 }
