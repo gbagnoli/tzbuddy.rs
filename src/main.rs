6bb12540f3a75528949e7cdd0189b9f2af22c089
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-10-26 11:50:32 +0000

    basic date parsing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
 extern crate clap;
+extern crate chrono;
+extern crate chrono_tz;
 
-fn get_timezones(cmdline_tzs: Option<clap::Values<'_>>) -> Vec<&str> {
-    let mut timezones = Vec::new();
+use std::collections::HashMap;
+use chrono_tz::Tz;
+use clap::{App, Values};
+use chrono::{DateTime, Utc};
+
+fn get_timezones(cmdline_tzs: Option<Values<'_>>) -> HashMap<&str, Tz> {
+    let mut timezones = HashMap::new();
     match cmdline_tzs {
         Some(values) => {
-            for tz in values {
-                timezones.push(tz);
+            for tz_str in values {
+                let tz_opt = tz_str.parse();
+                match tz_opt {
+                    Ok(tz) => {
+                        timezones.insert(tz_str, tz);
+                    }
+                    Err(e) => println!("{}: discarding", e),
+                }
             }
         }
-        None => {
-            println!("todo implement local time");
-        }
+        None => println!("No timezones provided."),
 
     }
     timezones
 }
 
+fn get_utc_date(cmdline_date: Option<&str>) -> DateTime<Utc> {
+    match cmdline_date {
+        Some(date) => {
+            let format = "%Y-%m-%d %H:%M";
+            match chrono::NaiveDateTime::parse_from_str(date, format) {
+                Ok(naive) => chrono::DateTime::from_utc(naive, chrono::Utc),
+                Err(e) => {
+                    println!(
+                        "Invalid date '{}' for format '{}' ({}). Using now()",
+                        date,
+                        format,
+                        e
+                    );
+                    chrono::Utc::now()
+                }
+            }
+        }
+        None => chrono::Utc::now(),
+    }
+}
+
 fn main() {
     let config_yaml = clap::load_yaml!("args.yaml");
-    let matches = clap::App::from(config_yaml).get_matches();
-
+    let matches = App::from(config_yaml).get_matches();
     let _timezones = get_timezones(matches.values_of("timezones"));
+    let date = get_utc_date(matches.value_of("date"));
+    println!("Using date: {}", date);
     println!("Hello, world!");
 }
