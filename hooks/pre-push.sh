1f58139020027a81643dbc39753b849b3f9c747d
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-10-28 20:45:08 +0000

    Add commands print to pre-push hook

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -4,16 +4,21 @@ set -eu
 set -o pipefail
 
 pushd "$(git rev-parse --show-toplevel)" >/dev/null
+echo "* Running cargo check"
 cargo check
 cargo check && \
+echo "* Running cargo fmt"
 cargo fmt -- --check && \
+echo "* Running cargo clippy"
 cargo clippy --all --all-targets -- -Dwarnings -Drust-2018-idioms && \
+echo "* Running cargo test"
 cargo test
 
+echo "* Running shellcheck"
 find . -name '*.sh' -print0 | xargs -0 shellcheck
 
 if [ -x "$(command -v circleci)" ]; then
-  echo "Validating CircleCI config"
+  echo "* Validating CircleCI config"
   circleci config validate .circleci/config.yml 
 fi
 popd >/dev/null
